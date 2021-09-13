d9837edf4c2de27e2000ce1f10e9c531e3f710f9
tripleh1801 <hoanghhh.01257989756@gmail.com>
2021-09-13 23:17:42 +0700

    spql

--- src/main/java/com/example/employeecnm_crud/repository/EmployeeRepository.java
+++ src/main/java/com/example/employeecnm_crud/repository/EmployeeRepository.java
@@ -2,7 +2,28 @@ package com.example.employeecnm_crud.repository;
 
 import com.example.employeecnm_crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+    @Query("select e from Employee e")
+    List<Employee> getAll();
 
+    @Transactional
+    @Modifying
+    @Query(value = "insert into employee(first_name,last_name) values(?1,?2)", nativeQuery = true)
+    void addEmployee(String firstName, String lastName);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update Employee e set e.firstName=?2, e.lastName=?3 where e.id = ?1")
+    void updateEmployee(int id ,String firstName, String lastName);
+
+    @Transactional
+    @Modifying
+    @Query("delete from Employee e where e.id = ?1")
+    void deleteEmployee(int id);
 }
---
+++ src/test/java/com/example/employeecnm_crud/Test_JPQL.java
@@ -0,0 +1,31 @@
+package com.example.employeecnm_crud;
+
+import com.example.employeecnm_crud.entity.Employee;
+import com.example.employeecnm_crud.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test_JPQL {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Test
+    public void getAll() {
+        System.out.println("getAll: " + employeeRepository.getAll());
+    }
+    @Test
+    public void add() {
+        employeeRepository.addEmployee("Test2 ", "Test1");
+    }
+    @Test
+    public void update() {
+        Employee employee = employeeRepository.findById(3).get();
+        employeeRepository.updateEmployee(3, "new name1 ", "new name2");
+    }
+
+    @Test
+    public void delete() {
+        employeeRepository.deleteEmployee(3);
+    }
+}
